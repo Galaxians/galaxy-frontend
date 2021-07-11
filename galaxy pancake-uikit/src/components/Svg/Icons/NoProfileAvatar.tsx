5a66bd6a390f18ccf2b15662290aed4323305946
amitknimje <amitknimje@gmail.com>
2021-07-12 00:53:57 +0530

    Added

--- galaxy pancake-uikit/src/components/Svg/Icons/NoProfileAvatar.tsx
+++ galaxy pancake-uikit/src/components/Svg/Icons/NoProfileAvatar.tsx
@@ -10,24 +10,15 @@ const Icon: React.FC<SvgProps> = (props) => {
 
   return (
     <Svg viewBox="0 0 32 32" {...props}>
-      {/* <path d="M32 16c0 8.837-7.163 16-16 16S0 24.837 0 16 7.163 0 16 0s16 7.163 16 16z" fill={primaryColor} />
-      <mask id="A" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="32" height="32">
-        <path d="M32 16c0 8.837-7.163 16-16 16S0 24.837 0 16 7.163 0 16 0s16 7.163 16 16z" fill="#c4c4c4" />
-      </mask>
-      <g mask="url(#A)">
-        <path
-          d="M25.128 16.436c0 3.115-4.133 5.641-9.231 5.641s-9.231-2.526-9.231-5.641V15h18.461v1.436zm2.205 13.806c0-3.815-5.074-6.908-11.333-6.908S4.667 26.426 4.667 30.242V32h22.667v-1.759z"
-          fill={secondaryColor}
-        />
-        <path
-          fillRule="evenodd"
-          d="M10.234 5.601C9.942 4.264 10.96 3 12.328 3c1.184 0 2.143.959 2.143 2.143v3.873l1.427-.067c.589 0 1.166.034 1.724.098V5.143c0-1.184.959-2.143 2.143-2.143 1.368 0 2.386 1.264 2.093 2.601l-.931 4.258c2.529 1.006 4.201 2.749 4.201 4.731 0 3.115-4.133 5.641-9.231 5.641s-9.231-2.526-9.231-5.641c0-2.053 1.794-3.849 4.476-4.836l-.908-4.153z"
-          fill={secondaryColor}
-        />
-        <ellipse cx="12.308" cy="14.846" rx="1.026" ry="1.538" fill={primaryColor} />
-        <ellipse cx="19.385" cy="14.846" rx="1.026" ry="1.538" fill={primaryColor} />
-      </g> */}
-      <image width="205" height="26" href={"/images/fresh/logomain.png"} />
+  <g id="Profile" transform="translate(-12 -12)">
+    <circle id="Ellipse_96" data-name="Ellipse 96" cx="16" cy="16" r="16" transform="translate(12 12)" fill="#f5f5f5"/>
+    <g id="Symbol_6_1" data-name="Symbol 6 – 1" transform="translate(20 20)">
+      <g id="Union_1" data-name="Union 1" transform="translate(0 -354)">
+        <circle id="Ellipse_3" data-name="Ellipse 3" cx="4" cy="4" r="4" transform="translate(4 354)" fill="#707070"/>
+        <path id="Path_6" data-name="Path 6" d="M8,10c-4.4,0-8,1.8-8,4v2H16V14C16,11.8,12.4,10,8,10Z" transform="translate(0 354)" fill="#707070"/>
+      </g>
+    </g>
+  </g>
     </Svg>
   );
 };
