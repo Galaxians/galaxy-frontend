701c63f4209da70a791e8c3587e992d8c5d93ab5
Techlord <dev@techlord.ned>
2021-08-23 08:00:40 -0700

    sidebar bug

--- src/pagecomponent/Sidebar.tsx
+++ src/pagecomponent/Sidebar.tsx
@@ -117,7 +117,7 @@ const Btn = ({ text}) => {
 export default function Sidebar({ isOpen, toggle }) {
     return (
         <>
-        <BackDiv onClick={isOpen ? toggle : null}/>
+        { isOpen ? <BackDiv onClick={isOpen ? toggle : null}/> : null }
         <SidebarContainer isOpen={isOpen}>
             <MenuLogo onClick={toggle}>
               <img src="./assets/img/menu.png" alt="menu"/>
